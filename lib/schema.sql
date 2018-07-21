df1c6f3432c65d031a08c12aaccb394596fbcfff
Adam Urbina <adamurbina@gmail.com>
2018-07-21 18:00:20 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,5 +2,10 @@ CREATE TABLE series(
     id INTEGER PRIMARY KEY,
     title TEXT,
     author_id INTEGER,
-    subgenre_id INTEGER,
+    subgenre_id INTEGER
+);
+
+CREATE TABLE subgenres(
+    name TEXT,
+    
 );
