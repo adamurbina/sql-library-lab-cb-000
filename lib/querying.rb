c5e2e2092d1161c89352c5fca13618f4c71fc3cb
Adam Urbina <adamurbina@gmail.com>
2018-07-21 19:32:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,9 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM characters
   LEFT JOIN series
-  ON series.id = characters.series_id;"
+  ON series.id = characters.series_id
+  WHERE characters.species = 'human'
+  ORDER BY COUNT(characters.species = 'human');"
 end
 
 def select_character_names_and_number_of_books_they_are_in
